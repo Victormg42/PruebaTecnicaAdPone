1e4bce1c22c6f5f6bb9aa32d97518d191bf7b877
Victor <victor@gmail.com>
2021-03-23 23:10:35 +0100

    Subida al repositorio de GitHub

---
+++ resources/views/index.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <script src="js/code.js"></script>
+</head>
+
+<body style="text-align: center; margin-top: 10%; background-color: #D0D3D4 ;">
+    <h2>AdPone</h2>
+    <h3>Prueba Técnica de Víctor Martínez</h3>
+    <input type="text" id="string">
+    <button onclick="getValue()">Enviar Datos</button>
+    <h2 id="msg"></h2>
+</body>
+
+</html>
